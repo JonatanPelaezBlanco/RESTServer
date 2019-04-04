68682f2ce0967f1fcda2edb3c49f8d8fd30088fc
Jonatan <jpelaezblanco@hotmail.com>
2019-04-04 18:06:52 +0200

    Correciones

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 
 app.use(require('./routes/usuario'));
 
-mongoose.connect(process.env.URLDB, (err, res) => {
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
     if (err) throw err;
 
