afa1f24a709352b4249daf6404ea84e1a2d085d8
Jonatan <jpelaezblanco@hotmail.com>
2019-04-04 16:51:43 +0200

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,17 @@
 //Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+//Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BBDD
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Jpelaezblanco:Qw3rty99@cluster0-9qape.mongodb.net/test?retryWrites=true'
+};
+
+
+process.env.URLDB = urlDB;
