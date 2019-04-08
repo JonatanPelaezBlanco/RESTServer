9c041f25ed4ba4156ecf11a8bdf4d8c7f96daf4f
Jonatan <jpelaezblanco@hotmail.com>
2019-04-08 10:51:43 +0200

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,13 @@ process.env.PORT = process.env.PORT || 3000;
 //Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+//Fecha de exiracion Token
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//Seed de autenticacion
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
+
+
 //BBDD
 let urlDB;
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,9 +4,11 @@ const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
 
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
+
 const app = express();
 
-app.get('/usuario', (req, res) => {
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -39,7 +41,7 @@ app.get('/usuario', (req, res) => {
         });
 });
 
-app.post('/usuario', (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
 
     let body = req.body;
 
@@ -66,7 +68,7 @@ app.post('/usuario', (req, res) => {
     });
 })
 
-app.put('/usuario/:id', (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
 
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
@@ -89,7 +91,7 @@ app.put('/usuario/:id', (req, res) => {
 
 })
 
-app.delete('/usuario/:id', (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
 
     let id = req.params.id;
 
